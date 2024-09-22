b7d34da26d216abb4bd4376591902e947f5c51d6
Golam Rabbani <dev.golamrabbani@gmail.com>
2024-09-22 15:55:56 +0530

    style redio groups and add custom color option

--- components/banner-generator.tsx
+++ components/banner-generator.tsx
@@ -13,7 +13,15 @@ import {
 } from "./ui/collapsible";
 import { Input } from "./ui/input";
 import { Separator } from "./ui/separator";
-import { AlignLeft, ChevronDown } from "lucide-react";
+import {
+  AlignCenter,
+  AlignLeft,
+  AlignRight,
+  Blend,
+  ChevronDown,
+  Shuffle,
+  Square,
+} from "lucide-react";
 import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
 import { Slider } from "./ui/slider";
 import {
@@ -37,7 +45,8 @@ type PaletteType =
   | "pastel"
   | "dark"
   | "grayscale"
-  | "vibrant";
+  | "vibrant"
+  | "custom";
 
 export default function BannerGenerator() {
   const [bannerUrl, setBannerUrl] = useState("");
@@ -48,7 +57,9 @@ export default function BannerGenerator() {
   const [palette, setPalette] = useState<PaletteType>("cool");
   const [patternIntensity, setPatternIntensity] = useState(0.1);
   const [fontSize, setFontSize] = useState(48);
-  const [showAdvanced, setShowAdvanced] = useState(false);
+  const [showAdvanced, setShowAdvanced] = useState(true);
+  const [customColor1, setCustomColor1] = useState("#000000");
+  const [customColor2, setCustomColor2] = useState("#ffffff");
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -58,14 +69,19 @@ export default function BannerGenerator() {
       const ctx = canvas.getContext("2d");
 
       if (ctx) {
-        // Color Count 50% Chance , If fillmode flat than only one color
-        const colorCount =
-          fillMode === "flat" ? 1 : Math.random() < 0.5 ? 2 : 3;
-        // Select colors
-        const selectedColorPalette = colorPalettes[palette];
-        const selectedColors = [...selectedColorPalette]
-          .sort(() => Math.random() - 0.5)
-          .slice(0, colorCount);
+        let selectedColors: string[];
+        if (palette === "custom") {
+          selectedColors = [customColor1, customColor2];
+        } else {
+          // Color Count 50% Chance , If fillmode flat than only one color
+          const colorCount =
+            fillMode === "flat" ? 1 : Math.random() < 0.5 ? 2 : 3;
+          // Select colors
+          const selectedColorPalette = colorPalettes[palette];
+          selectedColors = [...selectedColorPalette]
+            .sort(() => Math.random() - 0.5)
+            .slice(0, colorCount);
+        }
 
         // Is gradiant 70% chance, If fillmode gradient 100% chance
         const isGradient =
@@ -242,30 +258,46 @@ export default function BannerGenerator() {
                 onValueChange={(value: "left" | "center" | "right") =>
                   setAlign(value)
                 }
-                className="flex space-x-4 mt-1"
+                className="flex mt-2"
               >
-                <div className="flex items-center space-x-2">
+                <div className="flex items-center space-x-3">
                   <Label
                     htmlFor="left"
-                    className="flex flex-col justify-center"
+                    className="flex flex-col justify-center items-center bg-popover rounded-md w-16 h-14 gap-1 hover:bg-accent cursor-pointer hover:text-accent-foreground [&:has([data-state=checked])]:border-primary border-muted [&:has([data-state=checked])]:text-primary border-2"
                   >
-                    <AlignLeft />
-                    <span>Left</span>
+                    <AlignLeft className="h-5 w-5" />
+                    <span className="text-[0.7rem]">Left</span>
                     <RadioGroupItem
                       value="left"
                       id="left"
                       className="sr-only"
                     />
                   </Label>
+                  <Label
+                    htmlFor="center"
+                    className="flex flex-col justify-center items-center bg-popover rounded-md w-16 h-14 gap-1 hover:bg-accent cursor-pointer hover:text-accent-foreground [&:has([data-state=checked])]:border-primary border-muted [&:has([data-state=checked])]:text-primary border-2"
+                  >
+                    <AlignCenter className="h-5 w-5" />
+                    <span className="text-[0.7rem]">Center</span>
+                    <RadioGroupItem
+                      value="center"
+                      id="center"
+                      className="sr-only"
+                    />
+                  </Label>
+                  <Label
+                    htmlFor="right"
+                    className="flex flex-col justify-center items-center bg-popover rounded-md w-16 h-14 gap-1 hover:bg-accent cursor-pointer hover:text-accent-foreground [&:has([data-state=checked])]:border-primary border-muted [&:has([data-state=checked])]:text-primary border-2"
+                  >
+                    <AlignRight className="h-5 w-5" />
+                    <span className="text-[0.7rem]">Right</span>
+                    <RadioGroupItem
+                      value="right"
+                      id="right"
+                      className="sr-only"
+                    />
+                  </Label>
                 </div>
-                {/* <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="center" id="center" />
-                  <Label htmlFor="center">Center</Label>
-                </div>
-                <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="right" id="right" />
-                  <Label htmlFor="right">Right</Label>
-                </div> */}
               </RadioGroup>
             </div>
             <div>
@@ -275,20 +307,36 @@ export default function BannerGenerator() {
                 onValueChange={(value) =>
                   setFillMode(value as "auto" | "flat" | "gradient")
                 }
-                className="flex space-x-4 mt-1"
+                className="flex space-x-1 mt-1"
               >
-                <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="auto" id="auto" />
-                  <Label htmlFor="auto">Auto</Label>
-                </div>
-                <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="flat" id="flat" />
-                  <Label htmlFor="flat">Flat</Label>
-                </div>
-                <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="gradient" id="gradient" />
-                  <Label htmlFor="gradient">Gradient</Label>
-                </div>
+                <Label
+                  htmlFor="auto"
+                  className="flex flex-col justify-center items-center bg-popover rounded-md w-16 h-14 gap-1 hover:bg-accent cursor-pointer hover:text-accent-foreground [&:has([data-state=checked])]:border-primary border-muted [&:has([data-state=checked])]:text-primary border-2"
+                >
+                  <Shuffle className="h-5 w-5" />
+                  <span className="text-[0.7rem]">Auto</span>
+                  <RadioGroupItem value="auto" id="auto" className="sr-only" />
+                </Label>
+                <Label
+                  htmlFor="flat"
+                  className="flex flex-col justify-center items-center bg-popover rounded-md w-16 h-14 gap-1 hover:bg-accent cursor-pointer hover:text-accent-foreground [&:has([data-state=checked])]:border-primary border-muted [&:has([data-state=checked])]:text-primary border-2"
+                >
+                  <Square className="h-5 w-5" />
+                  <span className="text-[0.7rem]">Flat</span>
+                  <RadioGroupItem value="flat" id="flat" className="sr-only" />
+                </Label>
+                <Label
+                  htmlFor="gradient"
+                  className="flex flex-col justify-center items-center bg-popover rounded-md w-16 h-14 gap-1 hover:bg-accent cursor-pointer hover:text-accent-foreground [&:has([data-state=checked])]:border-primary border-muted [&:has([data-state=checked])]:text-primary border-2"
+                >
+                  <Blend className="h-5 w-5" />
+                  <span className="text-[0.7rem]">Gradient</span>
+                  <RadioGroupItem
+                    value="gradient"
+                    id="gradient"
+                    className="sr-only"
+                  />
+                </Label>
               </RadioGroup>
             </div>
             <div>
@@ -307,9 +355,30 @@ export default function BannerGenerator() {
                   <SelectItem value="vibrant">Vibrant</SelectItem>
                   <SelectItem value="dark">Dark</SelectItem>
                   <SelectItem value="pastel">Pastel</SelectItem>
+                  <SelectItem value="custom">Custom</SelectItem>
                 </SelectContent>
               </Select>
             </div>
+            {palette === "custom" && (
+              <div>
+                <Label>Custom color</Label>
+                <div className="flex gap-3 mt-2">
+                  <Input
+                    value={customColor1}
+                    onChange={(e) => setCustomColor1(e.target.value)}
+                    type="color"
+                    className="w-14 h-14 p-1"
+                  />
+                  <Input
+                    value={customColor2}
+                    onChange={(e) => setCustomColor2(e.target.value)}
+                    type="color"
+                    className="w-14 h-14 p-1"
+                  />
+                </div>
+              </div>
+            )}
+
             <div>
               <Label>Pattern Intensity:</Label>
               <Slider
